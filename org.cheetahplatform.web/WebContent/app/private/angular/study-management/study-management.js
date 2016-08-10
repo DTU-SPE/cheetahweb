e1a3ff8445b20e2d8b0fb0279853540c53b36ebd
stefan.zugal <contact@zugal.info>
2016-08-10 11:10:59 +0200

    Preparing infrastructure for data analysis, for #534

--- org.cheetahplatform.web/WebContent/app/private/angular/study-management/study-management.js
+++ org.cheetahplatform.web/WebContent/app/private/angular/study-management/study-management.js
@@ -1,64 +1,93 @@
-angular.module('cheetah.StudyManagement', ['ngRoute'])
-    .controller('StudyCtrl', function ($scope, $http, $q) {
-        $scope.studies = [];
-        $scope.newStudyName = "";
-        $scope.newStudyComment = "";
-        $scope.selection = {id: -1};
+angular.module('cheetah.StudyManagement', ['ngRoute']).controller('StudyCtrl', function ($scope, $http, $q) {
+    $scope.studies = [];
+    $scope.newStudyName = "";
+    $scope.newStudyComment = "";
+    $scope.selection = {id: -1};
+    $scope.mode = 'overview';
 
-        $http.get("../../private/listStudies").success(function (studies) {
-            $scope.studies = studies;
-        });
+    $http.get("../../private/listStudies").success(function (studies) {
+        $scope.studies = studies;
+    });
 
-        $scope.openAddStudyDialog = function () {
-            $('#addStudyDialog').modal();
-        };
+    $scope.openAddStudyDialog = function () {
+        $('#addStudyDialog').modal();
+    };
 
-        $scope.deleteStudy = function () {
-            BootstrapDialog.confirm('Do you really wan to delete the selected study?', function (result) {
-                if (result) {
-                    var postData = {studyId: $scope.selection.id};
-                    $http.post('../../private/deleteStudy', postData).success(function (studies) {
-                        $scope.studies = studies;
-                    }).error(function (error) {
-                        BootstrapDialog.show({
-                            title: 'Sorry, a problem occurred...',
-                            message: error.message,
-                            buttons: [{
-                                id: 'btn-ok',
-                                label: 'OK',
-                                cssClass: 'btn-primary',
-                                autospin: false,
-                                action: function (dialogRef) {
-                                    dialogRef.close();
-                                }
-                            }]
-                        });
+    $scope.deleteStudy = function () {
+        BootstrapDialog.confirm('Do you really wan to delete the selected study?', function (result) {
+            if (result) {
+                var postData = {studyId: $scope.selection.id};
+                $http.post('../../private/deleteStudy', postData).success(function (studies) {
+                    $scope.studies = studies;
+                }).error(function (error) {
+                    BootstrapDialog.show({
+                        title: 'Sorry, a problem occurred...',
+                        message: error.message,
+                        buttons: [{
+                            id: 'btn-ok',
+                            label: 'OK',
+                            cssClass: 'btn-primary',
+                            autospin: false,
+                            action: function (dialogRef) {
+                                dialogRef.close();
+                            }
+                        }]
                     });
-                }
-            });
+                });
+            }
+        });
+    };
+
+    $scope.setSelection = function (study) {
+        $scope.selection = study;
+    };
+
+    $scope.submitNewStudy = function () {
+        var postData = {
+            name: $scope.newStudyName,
+            comment: $scope.newStudyComment
         };
 
-        $scope.setSelection = function (study) {
-            $scope.selection = study;
-        };
+        $http.post("../../private/addStudy", angular.toJson(postData)).success(function (studies) {
+            $scope.studies = studies;
+            $('#addStudyDialog').modal('hide');
+            $scope.newStudyName = "";
+            $scope.newStudyComment = "";
+        });
+    };
 
-        $scope.submitNewStudy = function () {
-            var postData = {
-                name: $scope.newStudyName,
-                comment: $scope.newStudyComment
-            };
 
-            $http.post("../../private/addStudy", angular.toJson(postData)).success(function (studies) {
-                $scope.studies = studies;
-                $('#addStudyDialog').modal('hide');
-                $scope.newStudyName = "";
-                $scope.newStudyComment = "";
-            });
-        };
+    $scope.showDataProcessing = function () {
+        showModal($scope, 'cheetah-test-modal', $scope.selection);
+    };
 
-    }).config(function ($routeProvider) {
+    $scope.$on('cheetah-test-modal', function (event, study) {
+        $scope.selection = study;
+    });
+
+}).controller('TestController', function ($scope) {
+    $scope.$on('cheetah-test-modal', function (event, study) {
+        $scope.content = 'le content de la ' + study.name;
+    });
+
+    $scope.close = function () {
+        hideModal($scope, 'cheetah-test-modal', {id: -1});
+    };
+
+
+}).config(function ($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: 'angular/study-management/study-management.htm',
         controller: 'StudyCtrl'
     });
 });
+
+function hideModal($scope, id, data) {
+    $scope.$emit(id, data);
+    $('#' + id).modal('hide');
+}
+
+function showModal($scope, id, data) {
+    $('#' + id).modal('show');
+    $scope.$broadcast(id, data);
+}
