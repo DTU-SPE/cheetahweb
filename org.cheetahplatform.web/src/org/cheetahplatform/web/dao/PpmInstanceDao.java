1c62502c0def79b0842f693d339204ebb01022cc
Jakob Pinggera <Jakob.Pinggera@uibk.ac.at>
2017-03-10 09:52:50 +0100

    for #703 #709 new method for identifying ppm instances

--- org.cheetahplatform.web/src/org/cheetahplatform/web/dao/PpmInstanceDao.java
+++ org.cheetahplatform.web/src/org/cheetahplatform/web/dao/PpmInstanceDao.java
@@ -4,11 +4,35 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.cheetahplatform.web.dto.PpmInstanceDto;
 import org.cheetahplatform.web.servlet.AbstractCheetahServlet;
 
 public class PpmInstanceDao extends AbstractCheetahDao {
+
+	public List<PpmInstanceDto> selectProcessInstancesForSubjectAndTask(Connection connection, long subjectId, String experimentTask)
+			throws SQLException {
+		String query = "SELECT process_instance.database_id, process.id, process.database_id FROM process_instance join process on process.database_id = process_instance.process WHERE fk_subject = ? and process.id = ?;";
+		PreparedStatement statement = connection.prepareStatement(query);
+		statement.setLong(1, subjectId);
+		statement.setString(2, experimentTask);
+		ResultSet resultSet = statement.executeQuery();
+
+		List<PpmInstanceDto> ppmInstanceIds = new ArrayList<>();
+		while (resultSet.next()) {
+			long instanceId = resultSet.getLong("process_instance.database_id");
+			String process = resultSet.getString("process.id");
+			long processId = resultSet.getLong("process.database_id");
+			PpmInstanceDto ppmInstance = new PpmInstanceDto(instanceId, process, processId);
+			ppmInstanceIds.add(ppmInstance);
+		}
+
+		statement.close();
+
+		return ppmInstanceIds;
+	}
 
 	public PpmInstanceDto selectPpmInstance(long ppmInstanceId) throws SQLException {
 		Connection connection = AbstractCheetahServlet.getDatabaseConnection();
