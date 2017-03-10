ef048475311e2e7176160f3bace199669ad969a2
Ulrich Lobis <ulrich.lobis@uibk.ac.at>
2017-03-10 11:11:04 +0100

    organizing imports in AbstractConnectionWorkItem

--- org.cheetahplatform.web/src/org/cheetahplatform/web/eyetracking/AbstractConnectWorkItem.java
+++ org.cheetahplatform.web/src/org/cheetahplatform/web/eyetracking/AbstractConnectWorkItem.java
@@ -1,10 +1,5 @@
 package org.cheetahplatform.web.eyetracking;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.cheetahplatform.web.AbstractCheetahWorkItem;
 import org.cheetahplatform.web.dto.ConnectRequest;
 
